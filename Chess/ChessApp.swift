24961c930b31cc8b68432462a8a0059076f7f739
hamza5645 <hamzaosama5645@outlook.com>
2024-12-17 19:02:41 +0200

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Hamza Osama on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
